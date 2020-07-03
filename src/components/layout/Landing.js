795d246ebcca1c5370e4582410864c89c390a7af
davidbuzatu-marian <davidm.buzatu@gmail.com>
2020-07-03 15:58:29 +0300

    Implemented email sending service

---
+++ src/components/layout/Landing.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+import Contact from './Contact';
+
+const Landing = () => {
+  return (
+    <Fragment>
+      <Contact></Contact>
+    </Fragment>
+  );
+};
+
+export default Landing;
