c7ac53001c9cc439d2ba043f69f16fecc81a4213
davidbuzatu-marian <davidm.buzatu@gmail.com>
2020-07-04 18:53:17 +0300

    Added project image component

--- src/components/layout/Landing.js
+++ src/components/layout/Landing.js
@@ -2,13 +2,15 @@ import React, { Fragment } from 'react';
 import Contact from './Contact';
 import Navbar from '../navbar/Navbar';
 import Presentation from './Presentation';
+import Projects from './Projects';
 
 const Landing = () => {
   return (
     <Fragment>
       <Navbar></Navbar>
-      <Presentation></Presentation>
-      <Contact></Contact>
+      {/* <Presentation></Presentation> */}
+      <Projects></Projects>
+      {/* <Contact></Contact> */}
     </Fragment>
   );
 };
---
+++ src/components/layout/Projects.js
@@ -0,0 +1,36 @@
+import React, { Fragment } from 'react';
+import EllipseOrange from '../../img/EllipseOrange.png';
+import GroupEllipse from '../../img/GroupEllipse.png';
+import Project1 from '../../img/project-1.png';
+import Project2 from '../../img/project-2.png';
+import Project3 from '../../img/project-3.png';
+import ProjectIMG from '../utils/ProjectIMG';
+
+const Projects = () => {
+  const projectsIMG = [Project1, Project2, Project3];
+
+  return (
+    <Fragment>
+      <img
+        src={EllipseOrange}
+        alt='ellipse'
+        className='pos-absolute mt-72 ellipse-projects-img'
+      />
+      <div className='margin-72-auto text-center'>
+        <h1>My personal projects</h1>
+      </div>
+      <div className='container-projects'>
+        {projectsIMG.map((projectIMG) => (
+          <ProjectIMG image={projectIMG} />
+        ))}
+      </div>
+      <img
+        src={GroupEllipse}
+        alt='ellipse'
+        className='pos-absolute ellipse-group-projects-img'
+      />
+    </Fragment>
+  );
+};
+
+export default Projects;
