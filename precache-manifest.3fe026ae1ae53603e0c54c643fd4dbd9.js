8da2c03dd2081116f674a40575db1d9b3ef753f5
davidbuzatu-marian <davidm.buzatu@gmail.com>
2020-07-06 17:50:13 +0300

    Updates

---
+++ precache-manifest.3fe026ae1ae53603e0c54c643fd4dbd9.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb0cb772e6331bce37469d35b310175e",
+    "url": "/Portofolio-DavidB/index.html"
+  },
+  {
+    "revision": "8d2aedac85366dd9d332",
+    "url": "/Portofolio-DavidB/static/css/main.efb389c4.chunk.css"
+  },
+  {
+    "revision": "5e2d9fb90f45c24d9f94",
+    "url": "/Portofolio-DavidB/static/js/2.e39e09c5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Portofolio-DavidB/static/js/2.e39e09c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d2aedac85366dd9d332",
+    "url": "/Portofolio-DavidB/static/js/main.7ca12bec.chunk.js"
+  },
+  {
+    "revision": "aa2b3fb4d7007b62ecdd",
+    "url": "/Portofolio-DavidB/static/js/runtime-main.98293f79.js"
+  },
+  {
+    "revision": "c54706b0f9f6fdb8af554754f9fcc4b7",
+    "url": "/Portofolio-DavidB/static/media/about.c54706b0.svg"
+  },
+  {
+    "revision": "91df3dd6db577f4ceb0f791564197112",
+    "url": "/Portofolio-DavidB/static/media/asteroid.91df3dd6.png"
+  },
+  {
+    "revision": "983ef307c9b0ee1fabe38508d231fb2d",
+    "url": "/Portofolio-DavidB/static/media/contact.983ef307.svg"
+  },
+  {
+    "revision": "658ac2d58e82476b60ec78265054fa63",
+    "url": "/Portofolio-DavidB/static/media/deployment-illustration.658ac2d5.svg"
+  },
+  {
+    "revision": "0f0d206d8211ce5b81db01999c422eda",
+    "url": "/Portofolio-DavidB/static/media/dots-services.0f0d206d.png"
+  },
+  {
+    "revision": "a1f5e311d0a7349961952082fb6c82d4",
+    "url": "/Portofolio-DavidB/static/media/maintanance-illustration.a1f5e311.svg"
+  },
+  {
+    "revision": "87e3c4e596111d15395d0ef3af8419c5",
+    "url": "/Portofolio-DavidB/static/media/mobile-illustration.87e3c4e5.svg"
+  },
+  {
+    "revision": "eedc5847db0bc42916a4e55ac57a41f7",
+    "url": "/Portofolio-DavidB/static/media/portrait.eedc5847.png"
+  },
+  {
+    "revision": "119fae730e52185a7c58f344ac0c2bd1",
+    "url": "/Portofolio-DavidB/static/media/project-1.119fae73.png"
+  },
+  {
+    "revision": "2f84367c54d1d9db6349a3022469f1bc",
+    "url": "/Portofolio-DavidB/static/media/project-2.2f84367c.png"
+  },
+  {
+    "revision": "3651ddf503857cd6e1a1ca2f9100b1a1",
+    "url": "/Portofolio-DavidB/static/media/project-3.3651ddf5.png"
+  },
+  {
+    "revision": "1c0df33d876b5f814410f799c0c9abb9",
+    "url": "/Portofolio-DavidB/static/media/web-illustration.1c0df33d.svg"
+  }
+]);
